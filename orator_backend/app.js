1da66f1beae143414a83c843cb72a3ba8d526fb9
1712danish <1712danishali@gmail.com>
2020-07-22 11:48:18 +0530

    port changed

--- orator_backend/app.js
+++ orator_backend/app.js
@@ -18,6 +18,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(5000,function(){
+app.listen(process.env.PORT||5000,function(){
     console.log("Serever running on port 5000.")
 })
