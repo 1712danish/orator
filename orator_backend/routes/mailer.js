eadcd1a97b8830730319fbc10dcb0743362a03f2
1712danish <1712danishali@gmail.com>
2020-07-26 16:07:06 +0530

    cahged

--- orator_backend/routes/mailer.js
+++ orator_backend/routes/mailer.js
@@ -4,7 +4,7 @@ var nodemailer = require('nodemailer');
 exports.transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-           user: 'oratorlpu.pi@gmail.com',
+           user: 'oratorlpu.pi',
            pass: 'Orator@soc'
        }
    });
